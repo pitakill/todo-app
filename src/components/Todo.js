3558a99abe7021bd2a0577ef167948c4a68f8e5d
Leopoldo Caballero <git@pitakill.net>
2021-03-08 20:44:58 -0600

    Componente tipo clase (stateful)

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import '../css/Todo.css';
 
-function Todo() {
-  return (
-    <div className="list-item">
-      Tarea
-      <button className="delete is-pulled-right" />
-    </div>
-  )
-};
+class Todo extends React.Component {
+  state = {
+    done: true,
+  }
+
+  render () {
+    return (
+      <div className={`list-item ${this.state.done ? 'done' : ''}`}>
+        Tarea
+        <button className="delete is-pulled-right" />
+      </div>
+    )
+  }
+}
 
 export default Todo;
