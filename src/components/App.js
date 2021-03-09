31397aca06c1a437ec53f08eb0378f3f37fa7e44
Leopoldo Caballero <git@pitakill.net>
2021-03-08 20:24:45 -0600

    Reto 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Form from './Form';
+import TodoList from './TodoList';
 
 function App() {
   return (
     <div className="wrapper">
       <div className="card frame">
         <Header />
+        <TodoList />
         <Form />
       </div>
     </div>
