61f3c97145a921b1235fadb09c839878a35c73d6
Leopoldo Caballero <git@pitakill.net>
2021-03-10 16:47:43 -0600

    class to function component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,24 +3,22 @@ import PropTypes from 'prop-types';
 import '../css/Todo.css';
 import Checkmark from './Checkmark';
 
-class Todo extends React.Component {
-  render () {
-    return (
-      <div className={`list-item ${this.props.done ? 'done' : ''}`}>
-        {this.props.title}
-        <div className="is-pulled-right">
-          <Checkmark 
-            toggleFn={this.props.toggleFn}
-            done={this.props.done}
-          />
-          <button 
-            className="delete is-pulled-right"
-            onClick={e => this.props.deleteFn(e)}
-          />
-        </div>
+function Todo(props) {
+  return (
+    <div className={`list-item ${props.done ? 'done' : ''}`}>
+      {props.title}
+      <div className="is-pulled-right">
+        <Checkmark 
+          toggleFn={props.toggleFn}
+          done={props.done}
+        />
+        <button 
+          className="delete is-pulled-right"
+          onClick={e => props.deleteFn(e)}
+        />
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 Todo.propTypes = {
