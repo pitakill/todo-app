c56a50b64d5727ec8317d0d9f2ad8b480b9a0993
Leopoldo Caballero <git@pitakill.net>
2021-03-17 12:20:08 -0600

    Reto 5

--- src/components/App.js
+++ src/components/App.js
@@ -4,66 +4,92 @@ import Form from './Form';
 import TodoList from './TodoList';
 import '../css/App.css';
 
-class App extends React.Component {
-  state = { todos: [] }
+function App() {
+  // state = { todos: [] }
+  const [todos, setTodos] = React.useState([]);
+  const [show, setShow] = React.useState(true);
 
-  componentDidMount() {
-    this.setState({
-      todos: [
-        { title: "Sesión 1 (JSX)", done: true},
-        { title: "Sesión 2 (Estado y propiedades)", done: true },
-        { title: "Sesión 3 (Ciclo de vida)", done: true },
-        { title: "Sesión 4 (Hooks)", done: false },
-        { title: "Sesión 5 (Hooks)", done: false },
-        { title: "Sesión 6 (Rutas)", done: false },
-        { title: "Sesión 7 (PWA)", done: false },
-        { title: "Sesión 8 (Material UI)", done: false },
-      ],
-    })
+  // componentDidMount() {
+    // this.setState({
+      // todos: [
+        // { title: "Sesión 1 (JSX)", done: true},
+        // { title: "Sesión 2 (Estado y propiedades)", done: true },
+        // { title: "Sesión 3 (Ciclo de vida)", done: true },
+        // { title: "Sesión 4 (Hooks)", done: false },
+        // { title: "Sesión 5 (Hooks)", done: false },
+        // { title: "Sesión 6 (Rutas)", done: false },
+        // { title: "Sesión 7 (PWA)", done: false },
+        // { title: "Sesión 8 (Material UI)", done: false },
+      // ],
+    // })
+  // }
+  React.useEffect(() => {
+    setTodos([
+      { title: "Sesión 1 (JSX)", done: true},
+      { title: "Sesión 2 (Estado y propiedades)", done: true },
+      { title: "Sesión 3 (Ciclo de vida)", done: true },
+      { title: "Sesión 4 (Hooks)", done: false },
+      { title: "Sesión 5 (Hooks)", done: false },
+      { title: "Sesión 6 (Rutas)", done: false },
+      { title: "Sesión 7 (PWA)", done: false },
+      { title: "Sesión 8 (Material UI)", done: false },
+    ]);
+  }, []);
+
+  const handleClickDelete = (e, index) => {
+    // const todos = [...this.state.todos];
+    const t = [...todos];
+    // todos.splice(index, 1);
+    t.splice(index, 1);
+
+    // this.setState({ todos: todos });
+    setTodos(t);
   }
 
-  handleClickDelete = (e, index) => {
-    const todos = [...this.state.todos];
-    todos.splice(index, 1);
-
-    this.setState({ todos: todos });
-  }
-
-  handleClickToggleDone = (e, index) => {
-    const todos = [...this.state.todos];
+  const handleClickToggleDone = (e, index) => {
+    // const todos = [...this.state.todos];
+    const t = [...todos];
+    // todos[index].done = !todos[index].done;
     todos[index].done = !todos[index].done;
 
-    this.setState({ todos });
+    // this.setState({ todos });
+    setTodos(t);
   }
 
-  addTask = (title) => {
-    const exists = this.state.todos.find(e => title === e.title);
+  const addTask = (title) => {
+    // const exists = this.state.todos.find(e => title === e.title);
+    const exists = todos.find(e => title === e.title);
 
     if (exists) {
       alert(`La tarea "${title}" ya existe!`);
       return
     }
 
-    this.setState({
-      todos : this.state.todos.concat([{ title, done: false }])
-    });
+    // this.setState({
+      // todos : this.state.todos.concat([{ title, done: false }])
+    // });
+    setTodos(todos.concat([{ title, done: false }]));
   }
 
-  render() {
-    return (
-      <div className="wrapper">
-        <div className="card frame">
-          <Header counter={this.state.todos.length} />
-          <TodoList 
-            tasks={this.state.todos} 
-            toggleFn={this.handleClickToggleDone}
-            deleteFn={this.handleClickDelete}
-          />
-          <Form addTaskFn={this.addTask} />
-        </div>
+  const filtered = todos.filter(e => !e.done || e.done === show);
+
+  return (
+    <div className="wrapper">
+      <div className="card frame">
+        <Header
+          counter={filtered.length}
+          show={show}
+          toggleDone={setShow}
+        />
+        <TodoList 
+          tasks={filtered}
+          toggleFn={handleClickToggleDone}
+          deleteFn={handleClickDelete}
+        />
+        <Form addTaskFn={addTask} />
       </div>
-    );
-  }
+    </div>
+  )
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ShowHide from './ShowHide';
 import '../css/header.css';
 
 function Header(props) {
@@ -8,12 +9,15 @@ function Header(props) {
       <h1 className="card-header-title header">
         Hay {props.counter} tareas
       </h1>
+      <ShowHide show={props.show} toggleDone={props.toggleDone} />
     </div>
   )
 };
 
 Header.propTypes = {
-  counter: PropTypes.number
+  counter: PropTypes.number,
+  toggleDone: PropTypes.func.isRequired,
+  show: PropTypes.bool.isRequired,
 }
 
 Header.defaultProps = {
